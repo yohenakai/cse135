22a91b7a4e3bd3143202757f5cea2787b6be8f7d
hyeongminan <hyeongminan@gmail.com>
2014-04-22 10:42:04 -0700

    adding util class

---
+++ src/cse135/Util.java
@@ -0,0 +1,14 @@
+package cse135;
+
+public class Util {
+
+	
+	
+	public static final String SERVERNAME = "ec2-54-187-115-171.us-west-2.compute.amazonaws.com";
+	public static final String USERNAME = "ubuntu";
+	public static final String PASSWORD = "ubuntu";
+	public static final String DATABASE = "cse135";
+	public static final String PORTNUMBER = "5432";
+	
+	
+}
